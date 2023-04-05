23e7df203ac3dac0338c8c2499540b2f24bf042c
Will Morrison <will.morrison@starburstdata.com>
2023-04-04 22:06:33 -0400

    Refactor rule selector to standalone class and reload rules threadsafely

--- gateway-ha/src/main/java/io/trino/gateway/ha/router/RoutingGroupSelector.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/router/RoutingGroupSelector.java
@@ -1,16 +1,7 @@
 package io.trino.gateway.ha.router;
 
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.jeasy.rules.api.Facts;
-import org.jeasy.rules.api.Rules;
-import org.jeasy.rules.api.RulesEngine;
-import org.jeasy.rules.core.DefaultRulesEngine;
-import org.jeasy.rules.mvel.MVELRuleFactory;
-import org.jeasy.rules.support.reader.YamlRuleDefinitionReader;
 
 /**
  * RoutingGroupSelector provides a way to match an HTTP request to a Gateway routing group.
@@ -31,32 +22,7 @@ public interface RoutingGroupSelector {
    * to determine the right routing group.
    */
   static RoutingGroupSelector byRoutingRulesEngine(String rulesConfigPath) {
-    try {
-      RulesEngine rulesEngine = new DefaultRulesEngine();
-      MVELRuleFactory ruleFactory = new MVELRuleFactory(new YamlRuleDefinitionReader());
-      ConnectionChecker connectionChecker = new ConnectionChecker();
-      Logger.log.info("reading rules from {}", rulesConfigPath);
-      Rules rules = ruleFactory.createRules(
-          new FileReader(rulesConfigPath));
-
-      return request -> {
-        Logger.log.debug("Thread id {} : applying the routing rules",
-            Thread.currentThread().getId());
-        request.setAttribute("connectionChecker", connectionChecker);
-        Facts facts = new Facts();
-        HashMap<String, String> result = new HashMap<String, String>();
-        facts.put("request", request);
-        facts.put("result", result);
-        rulesEngine.fire(rules, facts);
-        return result.get("routingGroup");
-      };
-    } catch (Exception e) {
-      return request -> {
-        Logger.log.error("Error opening rules configuration file,"
-            + " using routing group header as default.", e);
-        return request.getHeader(ROUTING_GROUP_HEADER);
-      };
-    }
+    return new RuleReloadingRoutingGroupSelector(rulesConfigPath);
   }
 
   /**
