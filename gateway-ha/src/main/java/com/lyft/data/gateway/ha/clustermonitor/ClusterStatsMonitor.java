56f3b79d656e245ad8451c3217aad672bb953458
andythsu <f2280c@gmail.com>
2023-08-21 14:29:03 -0400

    deactive trino cluster if there are 0 workers available

---
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/clustermonitor/ClusterStatsMonitor.java
@@ -0,0 +1,7 @@
+package com.lyft.data.gateway.ha.clustermonitor;
+
+import com.lyft.data.gateway.ha.config.ProxyBackendConfiguration;
+
+public interface ClusterStatsMonitor {
+    ClusterStats monitor(ProxyBackendConfiguration backend);
+}
---
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/clustermonitor/ClusterStatsObserver.java
@@ -0,0 +1,21 @@
+package com.lyft.data.gateway.ha.clustermonitor;
+
+import java.util.List;
+
+import com.lyft.data.gateway.ha.router.BackendStateManager;
+
+public class ClusterStatsObserver implements PrestoClusterStatsObserver {
+
+    private BackendStateManager backendStateManager;
+    
+    public ClusterStatsObserver(BackendStateManager backendStateManager){
+        this.backendStateManager = backendStateManager;
+    }
+
+    @Override
+    public void observe(List<ClusterStats> clustersStats) {
+        for(ClusterStats clusterStats : clustersStats){
+            backendStateManager.updateStates(clusterStats.getClusterId(), clusterStats);
+        }
+    }
+}
