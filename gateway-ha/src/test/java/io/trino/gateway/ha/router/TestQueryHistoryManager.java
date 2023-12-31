e4eb375f695ab017c2c12f277ee0cfa183914bcf
Yuya Ebihara <ebyhry@gmail.com>
2023-12-31 22:46:21 +0900

    Extract JdbcConnectionManager creator method

--- gateway-ha/src/test/java/io/trino/gateway/ha/router/TestQueryHistoryManager.java
+++ gateway-ha/src/test/java/io/trino/gateway/ha/router/TestQueryHistoryManager.java
@@ -13,18 +13,16 @@
  */
 package io.trino.gateway.ha.router;
 
-import io.trino.gateway.ha.HaGatewayTestUtils;
-import io.trino.gateway.ha.config.DataStoreConfiguration;
 import io.trino.gateway.ha.persistence.JdbcConnectionManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
+import static io.trino.gateway.ha.TestingJdbcConnectionManager.createTestingJdbcConnectionManager;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -36,14 +34,7 @@ public class TestQueryHistoryManager
     @BeforeAll
     public void setUp()
     {
-        File baseDir = new File(System.getProperty("java.io.tmpdir"));
-        File tempH2DbDir = new File(baseDir, "h2db-" + System.currentTimeMillis());
-        tempH2DbDir.deleteOnExit();
-        HaGatewayTestUtils.seedRequiredData(
-                new HaGatewayTestUtils.TestConfig("", tempH2DbDir.getAbsolutePath()));
-        String jdbcUrl = "jdbc:h2:" + tempH2DbDir.getAbsolutePath();
-        DataStoreConfiguration db = new DataStoreConfiguration(jdbcUrl, "sa", "sa", "org.h2.Driver", 4);
-        JdbcConnectionManager connectionManager = new JdbcConnectionManager(db);
+        JdbcConnectionManager connectionManager = createTestingJdbcConnectionManager();
         queryHistoryManager = new HaQueryHistoryManager(connectionManager)
         {
         };
