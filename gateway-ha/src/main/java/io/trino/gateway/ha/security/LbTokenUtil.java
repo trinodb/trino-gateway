6346113a535ba316e837234f892e447922985df9
andythsu <f2280c@gmail.com>
2024-01-17 12:23:27 -0500

    added support for oauth2 audience

--- gateway-ha/src/main/java/io/trino/gateway/ha/security/LbTokenUtil.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/security/LbTokenUtil.java
@@ -16,10 +16,13 @@ package io.trino.gateway.ha.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Verification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.Optional;
 
 public final class LbTokenUtil
 {
@@ -33,7 +36,7 @@ public final class LbTokenUtil
     {
     }
 
-    public static boolean validateToken(String idToken, RSAPublicKey publicKey, String issuer)
+    public static boolean validateToken(String idToken, RSAPublicKey publicKey, String issuer, Optional<List<String>> audiences)
     {
         try {
             if (log.isDebugEnabled()) {
@@ -43,11 +46,13 @@ public final class LbTokenUtil
             }
 
             Algorithm algorithm = Algorithm.RSA256(publicKey, null);
-            JWT.require(algorithm)
-                    .withIssuer(issuer)
-                    .acceptLeeway(60 * 60) // Expired tokens are valid for an hour
-                    .build()
-                    .verify(idToken);
+            Verification verification =
+                    JWT.require(algorithm)
+                        .withIssuer(issuer);
+
+            audiences.ifPresent(auds -> verification.withAnyOfAudience(auds.toArray(new String[0])));
+
+            verification.build().verify(idToken);
         }
         catch (Exception exc) {
             log.error("Could not validate token.", exc);
