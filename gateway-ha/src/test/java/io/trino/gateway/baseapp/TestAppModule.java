4060d0e552fbb50295932a9d2293b2721ca86d5c
prakharsapre <prakhar.sapre2610@gmail.com>
2024-02-16 12:47:40 -0600

    fix unused imports

--- gateway-ha/src/test/java/io/trino/gateway/baseapp/TestAppModule.java
+++ gateway-ha/src/test/java/io/trino/gateway/baseapp/TestAppModule.java
@@ -1,26 +1,37 @@
 package io.trino.gateway.baseapp;
-
-import io.dropwizard.core.Configuration;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import io.dropwizard.core.server.DefaultServerFactory;
 import io.dropwizard.core.setup.Environment;
 import io.dropwizard.jetty.HttpConnectorFactory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
-import org.testcontainers.shaded.com.github.dockerjava.core.dockerfile.DockerfileStatement;
 
 import java.util.Collections;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class TestAppModule {
-
+public class TestAppModule
+{
     @Test
-    public void testGetApplicationPort() {
-
+    public void testGetApplicationPort()
+    {
         AppConfiguration mockConfig = Mockito.mock(AppConfiguration.class);
         DefaultServerFactory mockServerFactory = Mockito.mock(DefaultServerFactory.class);
         HttpConnectorFactory mockConnector = Mockito.mock(HttpConnectorFactory.class);
@@ -36,9 +47,9 @@ public class TestAppModule {
         assertEquals(8090, port);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void testGetApplicationPortThrowsException() {
-
+    @Test
+    public void testGetApplicationPortThrowsException()
+    {
         DefaultServerFactory defaultServerFactory = new DefaultServerFactory();
         defaultServerFactory.setApplicationConnectors(Collections.emptyList());
 
@@ -49,7 +60,6 @@ public class TestAppModule {
 
         AppModule<AppConfiguration, Environment> appModule = new AppModule<>(appConfiguration, environment) {};
 
-        appModule.getApplicationPort();
+        assertThrows(IllegalStateException.class, appModule::getApplicationPort);
     }
 }
-
