f159c8616fce69f8bf5897f3b15bfda906755b47
luis.costa <luis.costa@talkdesk.com>
2024-02-17 11:24:41 +0000

    remove docker-image folder

--- helm/docker-image/templates/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dockerize -template /opt/gateway/gateway-ha-config.yml:/opt/gateway/gateway-config.yml
-
-java -XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=80 \
-     --add-opens=java.base/java.lang=ALL-UNNAMED \
-     --add-opens=java.base/java.net=ALL-UNNAMED \
-     -jar gateway-ha.jar server gateway-config.yml
