93c694b75f00dd4dc81576e8c34bbd910043ade9
Star Poon <oneonestar@gmail.com>
2024-03-17 10:57:51 +0900

    Use JDBI for ExactMatchSourceSelectors

--- gateway-ha/src/main/java/io/trino/gateway/ha/persistence/dao/ExactMatchSourceSelectors.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/persistence/dao/ExactMatchSourceSelectors.java
@@ -13,66 +13,21 @@
  */
 package io.trino.gateway.ha.persistence.dao;
 
-import org.javalite.activejdbc.Model;
-import org.javalite.activejdbc.annotations.Cached;
-import org.javalite.activejdbc.annotations.CompositePK;
-import org.javalite.activejdbc.annotations.Table;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.util.Objects.requireNonNull;
 
-import static io.trino.gateway.ha.router.ResourceGroupsManager.ExactSelectorsDetail;
-
-@CompositePK({"environment", "source", "query_type"})
-@Table("exact_match_source_selectors") // located in gateway-ha-persistence-*.sql
-@Cached
-public class ExactMatchSourceSelectors
-        extends Model
+public record ExactMatchSourceSelectors(
+        @ColumnName("resource_group_id") String resourceGroupId,
+        @ColumnName("update_time") String updateTime,
+        @ColumnName("source") String source,
+        @ColumnName("environment") String environment,
+        @ColumnName("query_type") String queryType)
 {
-    private static final String resourceGroupId = "resource_group_id";
-    private static final String updateTime = "update_time";
-
-    private static final String source = "source";
-    private static final String environment = "environment";
-    private static final String queryType = "query_type";
-
-    /**
-     * Returns the most specific exact-match selector for a given environment, source and query type.
-     * NULL values in the environment and query type fields signify wildcards.
-     *
-     * @return List of ExactMatchSourceSelectors
-     */
-    public static List<ExactSelectorsDetail> upcast(
-            List<ExactMatchSourceSelectors> exactMatchSourceSelectorsList)
+    public ExactMatchSourceSelectors
     {
-        List<ExactSelectorsDetail> exactSelectors = new ArrayList<>();
-        for (ExactMatchSourceSelectors dao : exactMatchSourceSelectorsList) {
-            ExactSelectorsDetail exactSelectorDetail = new ExactSelectorsDetail();
-            exactSelectorDetail.setResourceGroupId(dao.getString(resourceGroupId));
-            exactSelectorDetail.setUpdateTime(dao.getString(updateTime));
-
-            exactSelectorDetail.setSource(dao.getString(source));
-            exactSelectorDetail.setEnvironment(dao.getString(environment));
-            exactSelectorDetail.setQueryType(dao.getString(queryType));
-
-            exactSelectors.add(exactSelectorDetail);
-        }
-        return exactSelectors;
-    }
-
-    /**
-     * Create a new exactMatchSourceSelector.
-     */
-    public static void create(
-            ExactMatchSourceSelectors model, ExactSelectorsDetail exactSelectorsDetail)
-    {
-        model.set(resourceGroupId, exactSelectorsDetail.getResourceGroupId());
-        model.set(updateTime, exactSelectorsDetail.getUpdateTime());
-
-        model.set(source, exactSelectorsDetail.getSource());
-        model.set(environment, exactSelectorsDetail.getEnvironment());
-        model.set(queryType, exactSelectorsDetail.getQueryType());
-
-        model.insert();
+        requireNonNull(resourceGroupId);
+        requireNonNull(updateTime);
+        requireNonNull(source);
     }
 }
