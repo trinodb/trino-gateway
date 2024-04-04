c10405a5ffd55088457a862e004221579c3214e6
Star Poon <oneonestar@gmail.com>
2024-04-04 22:30:58 +0900

    Simplify module class inheritance

--- gateway-ha/src/main/java/io/trino/gateway/ha/module/ClusterStateListenerModule.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/module/ClusterStateListenerModule.java
@@ -14,10 +14,9 @@
 package io.trino.gateway.ha.module;
 
 import com.google.common.collect.ImmutableList;
+import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import io.dropwizard.core.setup.Environment;
-import io.trino.gateway.baseapp.AppModule;
 import io.trino.gateway.ha.clustermonitor.ActiveClusterMonitor;
 import io.trino.gateway.ha.clustermonitor.ClusterStatsObserver;
 import io.trino.gateway.ha.clustermonitor.HealthCheckObserver;
@@ -33,13 +32,12 @@ import io.trino.gateway.ha.router.RoutingManager;
 import java.util.List;
 
 public class ClusterStateListenerModule
-        extends AppModule<HaGatewayConfiguration, Environment>
+        extends AbstractModule
 {
     MonitorConfiguration monitorConfig;
 
-    public ClusterStateListenerModule(HaGatewayConfiguration config, Environment env)
+    public ClusterStateListenerModule(HaGatewayConfiguration config)
     {
-        super(config, env);
         monitorConfig = config.getMonitor();
     }
 
