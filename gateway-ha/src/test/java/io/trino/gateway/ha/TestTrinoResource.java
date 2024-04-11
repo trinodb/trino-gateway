e340f71d45d92f614deee1519ac102af87189381
Star Poon <ypoon@lycorp.co.jp>
2024-04-11 15:02:57 +0900

    Switch to Airlift bootstrap

--- gateway-ha/src/test/java/io/trino/gateway/ha/TestTrinoResource.java
+++ gateway-ha/src/test/java/io/trino/gateway/ha/TestTrinoResource.java
@@ -69,7 +69,7 @@ public class TestTrinoResource
         prepareData();
 
         // Start Gateway
-        String[] args = {"server", testConfig.configFilePath()};
+        String[] args = {testConfig.configFilePath()};
         HaGatewayLauncher.main(args);
     }
 
