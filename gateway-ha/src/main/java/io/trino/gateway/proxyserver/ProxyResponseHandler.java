da71168b2f2c39a425bafa306bc95979978e9bee
Star Poon <ypoon@lycorp.co.jp>
2024-06-03 11:35:24 +0900

    Implement proxy server using Airlift

---
+++ gateway-ha/src/main/java/io/trino/gateway/proxyserver/ProxyResponseHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.gateway.proxyserver;
+
+import com.google.common.collect.ListMultimap;
+import io.airlift.http.client.HeaderName;
+import io.airlift.http.client.Request;
+import io.airlift.http.client.Response;
+import io.airlift.http.client.ResponseHandler;
+import io.trino.gateway.proxyserver.ProxyResponseHandler.ProxyResponse;
+
+import java.io.IOException;
+
+import static com.google.common.io.ByteStreams.toByteArray;
+import static java.util.Objects.requireNonNull;
+
+public class ProxyResponseHandler
+        implements ResponseHandler<ProxyResponse, RuntimeException>
+{
+    @Override
+    public ProxyResponse handleException(Request request, Exception exception)
+    {
+        throw new ProxyException("Request to remote Trino server failed", exception);
+    }
+
+    @Override
+    public ProxyResponse handle(Request request, Response response)
+    {
+        try {
+            return new ProxyResponse(response.getStatusCode(), response.getHeaders(), toByteArray(response.getInputStream()));
+        }
+        catch (IOException e) {
+            throw new ProxyException("Failed reading response from remote Trino server", e);
+        }
+    }
+
+    public record ProxyResponse(
+            int statusCode,
+            ListMultimap<HeaderName, String> headers,
+            byte[] body)
+    {
+        public ProxyResponse
+        {
+            requireNonNull(headers, "headers is null");
+        }
+    }
+}
---
+++ gateway-ha/src/main/java/io/trino/gateway/proxyserver/RouteToBackendResource.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.gateway.proxyserver;
+
+import com.google.inject.Inject;
+import io.trino.gateway.ha.handler.ProxyHandlerStats;
+import io.trino.gateway.ha.handler.RoutingTargetHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.container.AsyncResponse;
+import jakarta.ws.rs.container.Suspended;
+import jakarta.ws.rs.core.Context;
+
+import java.net.URI;
+
+import static io.trino.gateway.ha.handler.QueryIdCachingProxyHandler.V1_STATEMENT_PATH;
+import static io.trino.gateway.proxyserver.RouterPreMatchContainerRequestFilter.ROUTE_TO_BACKEND;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Handles requests that need to be routed to a Trino backend.
+ *
+ * @see RouterPreMatchContainerRequestFilter
+ */
+@Path(ROUTE_TO_BACKEND)
+public class RouteToBackendResource
+{
+    private final ProxyHandlerStats proxyHandlerStats;
+    private final ProxyRequestHandler proxyRequestHandler;
+    private final RoutingTargetHandler routingTargetHandler;
+
+    @Inject
+    public RouteToBackendResource(
+            ProxyHandlerStats proxyHandlerStats,
+            ProxyRequestHandler proxyRequestHandler,
+            RoutingTargetHandler routingTargetHandler)
+    {
+        this.proxyHandlerStats = requireNonNull(proxyHandlerStats);
+        this.proxyRequestHandler = requireNonNull(proxyRequestHandler);
+        this.routingTargetHandler = requireNonNull(routingTargetHandler);
+    }
+
+    @POST
+    public void postHandler(
+            String body,
+            @Context HttpServletRequest servletRequest,
+            @Suspended AsyncResponse asyncResponse)
+    {
+        if (servletRequest.getRequestURI().startsWith(V1_STATEMENT_PATH)) {
+            proxyHandlerStats.recordRequest();
+        }
+        String remoteUri = routingTargetHandler.getRoutingDestination(servletRequest);
+        proxyRequestHandler.postRequest(body, servletRequest, asyncResponse, URI.create(remoteUri));
+    }
+
+    @GET
+    public void getHandler(
+            @Context HttpServletRequest servletRequest,
+            @Suspended AsyncResponse asyncResponse)
+    {
+        String remoteUri = routingTargetHandler.getRoutingDestination(servletRequest);
+        proxyRequestHandler.getRequest(servletRequest, asyncResponse, URI.create(remoteUri));
+    }
+}
