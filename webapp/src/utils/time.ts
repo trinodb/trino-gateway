d3136e8acb27c87eb8ef13d0f82d19661ed37745
ytwp <ytwppp@163.com>
2024-07-18 12:52:13 +0800

    Improve the time of the UI distribution graph (#369)

--- webapp/src/utils/time.ts
+++ webapp/src/utils/time.ts
@@ -29,8 +29,7 @@ const format = (
     .format(formatString);
 };
 
-export const formatYYYYMMddHHMMSS = (timestamp: number): string => {
-  const date = new Date(timestamp);
+const formatDateComponents = (date: Date): string => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
@@ -38,4 +37,14 @@ export const formatYYYYMMddHHMMSS = (timestamp: number): string => {
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+export const formatTimestamp = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  return formatDateComponents(date);
+}
+
+export const formatZonedDateTime = (zonedDateTime: string): string => {
+  const date = new Date(zonedDateTime);
+  return formatDateComponents(date);
 }
