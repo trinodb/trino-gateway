531929a3377a1c8d9d0ba6bafa7b946e55c6d4bc
Pádraig O'Sullivan <osullivan.padraig@gmail.com>
2025-01-10 16:13:51 -0500

    Use flyway to perform database migrations on startup

---
+++ gateway-ha/src/main/java/io/trino/gateway/ha/persistence/FlywayMigration.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.gateway.ha.persistence;
+
+import io.airlift.log.Logger;
+import io.trino.gateway.ha.config.DataStoreConfiguration;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
+
+import static java.lang.String.format;
+
+public class FlywayMigration
+{
+    private static final Logger log = Logger.get(FlywayMigration.class);
+
+    private FlywayMigration() {}
+
+    private static String getLocation(String configDbUrl)
+    {
+        if (configDbUrl.startsWith("jdbc:postgresql")) {
+            return "postgresql";
+        }
+        if (configDbUrl.startsWith("jdbc:mysql")) {
+            return "mysql";
+        }
+        throw new IllegalArgumentException(format("Invalid JDBC URL: %s. Only PostgreSQL and MySQL are supported.", configDbUrl));
+    }
+
+    public static void migrate(DataStoreConfiguration config)
+    {
+        if (!config.isRunMigrationsEnabled()) {
+            log.info("Skip migrations as automatic migrations are disabled");
+            return;
+        }
+        log.info("Performing migrations...");
+        Flyway flyway = Flyway.configure()
+                .dataSource(config.getJdbcUrl(), config.getUser(), config.getPassword())
+                .locations(getLocation(config.getJdbcUrl()))
+                .baselineOnMigrate(true)
+                .baselineVersion("0")
+                .load();
+
+        MigrateResult migrations = flyway.migrate();
+        log.info("Performed %s migrations", migrations.migrationsExecuted);
+    }
+}
--- gateway-ha/src/test/java/io/trino/gateway/ha/TestingJdbcConnectionManager.java
+++ gateway-ha/src/test/java/io/trino/gateway/ha/TestingJdbcConnectionManager.java
@@ -30,7 +30,7 @@ public final class TestingJdbcConnectionManager
         tempH2DbDir.deleteOnExit();
         String jdbcUrl = "jdbc:h2:" + tempH2DbDir.getAbsolutePath();
         HaGatewayTestUtils.seedRequiredData(new HaGatewayTestUtils.TestConfig("", tempH2DbDir.getAbsolutePath()));
-        DataStoreConfiguration db = new DataStoreConfiguration(jdbcUrl, "sa", "sa", "org.h2.Driver", 4);
+        DataStoreConfiguration db = new DataStoreConfiguration(jdbcUrl, "sa", "sa", "org.h2.Driver", 4, false);
         Jdbi jdbi = Jdbi.create(jdbcUrl, "sa", "sa");
         return new JdbcConnectionManager(jdbi, db);
     }
