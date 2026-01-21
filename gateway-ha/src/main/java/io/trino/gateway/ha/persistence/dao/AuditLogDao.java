95fd8bfed531272fb18b946f4285752a7aba6cda
kjsbot <wang.fiona03@gmail.com>
2026-01-21 15:18:03 -0800

    Update table columns

--- gateway-ha/src/main/java/io/trino/gateway/ha/persistence/dao/AuditLogDao.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/persistence/dao/AuditLogDao.java
@@ -22,14 +22,14 @@ public interface AuditLogDao
 {
     @SqlUpdate("""
             INSERT INTO gateway_audit_logs (user_name, ip_address, backend_name, operation, context, success, user_comment, change_time)
-            VALUES (:user_name, :ip_address, :backend_name, :operation, :context, :success, :user_comment, :change_time)
+            VALUES (:user_name, :ip_address, :backend_name, :operation, :context, :success, LEFT(:user_comment, 1024), :change_time)
             """)
     void log(@Bind("user_name") String user_name,
             @Bind("ip_address") String ip_address,
             @Bind("backend_name") String backend_name,
             @Bind("operation") String operation,
             @Bind("context") String context,
-            @Bind("success") int success,
+            @Bind("success") Boolean success,
             @Bind("user_comment") String user_comment,
             @Bind("change_time") Timestamp change_time);
 }
